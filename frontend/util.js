1a2a7724fa3f56e94b8e59aeabd47a8b18b8bf9c
hancel.lin <imlinhanchao@foxmail.com>
2020-10-07 16:56:29 +0800

    新增format和open工具函數

--- frontend/util.js
+++ frontend/util.js
@@ -2,8 +2,17 @@ import config from '../config.json'
 
 let util = {
     title(title) {
-        title = title ? title + ' - ' + config.base.name: config.base.name;
+        title = title ? title + ' | ' + config.base.name: config.base.name;
         window.document.title = title;
+    },
+    open(url) {
+        window.open(url);
+    },
+    format(tpl, data) {
+        Object.keys(data).forEach(k => {
+            tpl = tpl.replace(new RegExp(`{{${k}}}`, 'g'), data[k]);
+        })
+        return tpl;
     }
 };
 
